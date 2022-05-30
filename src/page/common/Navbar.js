60f973fa8844c57e1eaae5ca9b2599a61606ed65
RHsanto <rakibulsanto789@gmail.com>
2022-05-30 21:55:32 +0600

    [DONE] navbar section

--- src/page/common/Navbar.js
+++ src/page/common/Navbar.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './common.css'
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar
-Navbar</h1>
+    <div className='menu-section'>
+      <div className="container">
+        <div className="d-flex justify-content-between align-items-center menu-bar">
+          <div className="logo">
+            <h1>LOGO</h1>
+          </div>
+          <div className=" d-flex align-items-center">
+            <div className="menu">
+          <Link to='/'><li>HOME</li></Link>
+          <Link to='#'><li>ABOUT</li></Link>
+          <Link to='#'><li>SERVICES</li></Link>
+          <Link to='#'><li>BLOG</li></Link>
+          <Link to='#'><li>CONTACT US</li></Link>
+            </div>
+
+            <div>
+             <button className='login'>LOGIN</button>
+             <button className='signup'>SIGNUP</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
