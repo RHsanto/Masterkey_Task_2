7b8ba417c0df85c3655340527e30245a3a83db78
RHsanto <rakibulsanto789@gmail.com>
2022-05-31 02:26:30 +0600

    [add] done

--- src/page/common/Footer.js
+++ src/page/common/Footer.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaInstagram,FaFacebookF ,FaYoutube} from "react-icons/fa";
+import {SiMarketo} from "react-icons/si";
 import './common.css'
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer
-Footer</h1>
+    <div className='footer'>
+     <div className="container">
+       <div className="row">
+         <div className="col-lg-4">
+          <h2>MARKET <SiMarketo/></h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero vel numquam officia
+                nobis in esse odio consectetur libero vitae ab.</p>
+              <div className="d-flex social-icons gap-3">
+               <li className='instagram'><FaInstagram/></li>
+               <li className='facebook'><FaFacebookF/></li>
+               <li className='youtube'><FaYoutube/></li>
+              </div>
+         </div>
+         <div className="col-lg-2">
+         <h5>Active Link</h5>
+              <div>
+               <li><Link to='/#'>ABOUT</Link> </li>
+               <li><Link to='/#'>PRODUCTS</Link> </li>
+               <li><Link to='/#'>BLOG</Link> </li>
+               <li><Link to='/#'> CONTACT US</Link></li>
+              </div>
+         </div>
+         <div className="col-lg-2">
+         <h5>Active Link</h5>
+              <div>
+               <li><Link to='/#'>SIGN UP</Link> </li>
+               <li><Link to='/#'>LOG IN</Link> </li>
+               <li><Link to='/#'> ADVERTISE</Link></li>
+              </div>
+         </div>
+         <div className="col-lg-3">
+           <h4>Search</h4>
+           <input type="search" name="" id="" />
+         </div>
+         
+       </div>
+     </div>
     </div>
   );
 };
--- src/page/common/Navbar.js
+++ src/page/common/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {SiMarketo} from "react-icons/si";
+import {RiBarChartHorizontalLine} from "react-icons/ri";
 import './common.css'
 const Navbar = () => {
   return (
     <div className='menu-section'>
       <div className="container">
         <div className="d-none d-lg-flex justify-content-between align-items-center menu-bar">
-          <div className="logo">
-            <h1>LOGO</h1>
+          <div className="logo text-light">
+            <h2>MARKET <SiMarketo/></h2>
           </div>
           <div className=" d-flex align-items-center">
             <div className="menu">
@@ -25,9 +27,31 @@ const Navbar = () => {
           </div>
         </div>
         {/* for mobile */}
-        <div className="d-flex d-lg-none">
+        <div className=" text-light d-flex d-lg-none py-2 justify-content-between">
           <div className="logo">
-            <h1>LOGO</h1>
+          <h2>MARKET <SiMarketo/></h2>
+          </div>
+          <div>
+         
+          <a class="btn text-light" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+          <RiBarChartHorizontalLine className='fs-2'/>
+          </a>
+
+
+<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title text-dark" id="offcanvasExampleLabel">Menu</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+     aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body ">
+   <Link to='/'><li class="text-reset" data-bs-dismiss="offcanvas"aria-label="Close">HOME</li></Link>
+   <Link to='#'><li class="text-reset" data-bs-dismiss="offcanvas"aria-label="Close">ABOUT</li></Link>
+   <Link to='#'><li class="text-reset" data-bs-dismiss="offcanvas"aria-label="Close">SERVICES</li></Link>
+   <Link to='#'><li class="text-reset" data-bs-dismiss="offcanvas"aria-label="Close">BLOG</li></Link>
+   <Link to='#'><li class="text-reset" data-bs-dismiss="offcanvas"aria-label="Close">CONTACT US</li></Link>
+  </div>
+</div>
           </div>
         </div>
       </div>
